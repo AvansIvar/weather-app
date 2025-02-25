b2cee4b1e86cf88e195ce69b289e7593c3c6c8e8
AvansIvar <igm.schreutelkamp@student.avans.nl>
2025-02-25 11:58:24 +0100

    Initial Commit

---
+++ weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Ivar Schreutelkamp on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ weather-appTests/weather_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appTests.swift
+//  weather-appTests
+//
+//  Created by Ivar Schreutelkamp on 25/02/2025.
+//
+
+import Testing
+@testable import weather_app
+
+struct weather_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
